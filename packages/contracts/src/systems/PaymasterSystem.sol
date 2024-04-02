29afb044ee4d8bc4f7f43eaf576278cc5f297e18
alvrs <alvarius@lattice.xyz>
2024-04-02 14:57:48 +0200

    use eth-infinitism interfaces from github, add testing scaffold from pimlico

--- packages/contracts/src/systems/PaymasterSystem.sol
+++ packages/contracts/src/systems/PaymasterSystem.sol
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.24;
 
+import { IPaymaster } from "@account-abstraction/contracts/interfaces/IPaymaster.sol";
+import { PackedUserOperation } from "@account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+import { IEntryPoint } from "@account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import { _packValidationData } from "@account-abstraction/contracts/core/Helpers.sol";
+
 import { System } from "@latticexyz/world/src/System.sol";
 import { Balances } from "@latticexyz/world/src/codegen/index.sol";
 import { ROOT_NAMESPACE_ID } from "@latticexyz/world/src/constants.sol";
-import { IPaymaster, PostOpMode } from "../eip4337/IPaymaster.sol";
-import { PackedUserOperation } from "../eip4337/PackedUserOperation.sol";
-import { IEntryPoint } from "../eip4337/IEntryPoint.sol";
-import { _packValidationData } from "../eip4337/Helpers.sol";
 import { EntryPoint } from "../codegen/index.sol";
 
 contract PaymasterSystem is System, IPaymaster {
@@ -33,18 +34,22 @@ contract PaymasterSystem is System, IPaymaster {
         uint256 maxCost
     ) internal virtual returns (bytes memory context, uint256 validationData) {
       (userOp, userOpHash, maxCost, context, validationData);
-      // TODO
+      // Require the sender to be a valid spender of a user account
+      // Require the user account balance associated with the app smart account to have sufficient balance
+      // Deduct gasPrice * (verificationGasLimit + callGasLimit + paymasterVerificationGasLimit + postOpGasLimit) from the user account balance
+      // Pass the deducted balance in context
       return ("", _packValidationData(false, 0, 0));
     }
 
     /// @inheritdoc IPaymaster
     function postOp(
-        PostOpMode mode,
+        IPaymaster.PostOpMode mode,
         bytes calldata context,
-        uint256 actualGasCost
+        uint256 actualGasCost,
+        uint256 actualUserOpFeePerGas
     ) external override {
         _requireFromEntryPoint();
-        _postOp(mode, context, actualGasCost);
+        _postOp(mode, context, actualGasCost, actualUserOpFeePerGas);
     }
 
     /**
@@ -61,12 +66,27 @@ contract PaymasterSystem is System, IPaymaster {
      * @param actualGasCost - Actual gas used so far (without this postOp call).
      */
     function _postOp(
-        PostOpMode mode,
+        IPaymaster.PostOpMode mode,
         bytes calldata context,
-        uint256 actualGasCost
+        uint256 actualGasCost,
+        uint256 actualUserOpFeePerGas
     ) internal virtual {
-        (mode, context, actualGasCost); // unused params
+        (mode, context, actualGasCost, actualUserOpFeePerGas); // unused params
         revert("not implemented");
+        // Refund decutedBalance - (actualGasCost * actualUserOpFeePerGas + overhead for postOp) to the user account balance
+    }
+
+    function depositTo(address account) public payable {
+      // Increase balance for this account
+      // Deposit the value in the entry point
+    }
+
+    function registerSpender(address spender) public {
+      // Add a new spender to the spender table
+    }
+
+    function unregisterSpender(address spender) public {
+      // Remove a spender from the spender table
     }
 
     /**
@@ -83,7 +103,11 @@ contract PaymasterSystem is System, IPaymaster {
      * Add a deposit for this paymaster, used for paying for transaction fees.
      */
     function deposit() public payable {
-        Balances.set(ROOT_NAMESPACE_ID, Balances.get(ROOT_NAMESPACE_ID) - _msgValue());
-        IEntryPoint(EntryPoint.get()).depositTo{value: _msgValue()}(address(this));
+      _depositToEntryPoint(_msgValue());
+    }
+
+    function _depositToEntryPoint(uint256 amount) internal {
+        Balances.set(ROOT_NAMESPACE_ID, Balances.get(ROOT_NAMESPACE_ID) - amount);
+        IEntryPoint(EntryPoint.get()).depositTo{value: amount}(address(this)); 
     }
 }
---
+++ packages/contracts/test/PaymasterTest.t.sol
@@ -0,0 +1,126 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@account-abstraction/contracts/core/EntryPoint.sol";
+import "@account-abstraction/contracts/core/EntryPointSimulations.sol";
+import "@account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+import "@account-abstraction/contracts/samples/SimpleAccountFactory.sol";
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+
+import { MudTest } from "@latticexyz/world/test/MudTest.t.sol";
+import { NamespaceOwner } from "@latticexyz/world/src/codegen/tables/NamespaceOwner.sol";
+import { ROOT_NAMESPACE_ID } from "@latticexyz/world/src/constants.sol";
+import { IWorld } from "../src/codegen/world/IWorld.sol";
+import { EntryPoint as EntryPointTable } from "../src/codegen/tables/EntryPoint.sol";
+
+import { TestCounter} from "./utils/TestCounter.sol";
+import { BytesLib } from "./utils/BytesLib.sol";
+
+using ECDSA for bytes32;
+
+contract PaymasterTest is MudTest {
+  EntryPoint entryPoint;
+  EntryPointSimulations entryPointSimulations;
+  SimpleAccountFactory accountFactory;
+  IWorld paymaster;
+  TestCounter counter;
+
+  address payable beneficiary;
+  address paymasterOperator;
+  address user;
+  uint256 userKey;
+  address guarantor;
+  uint256 guarantorKey;
+  SimpleAccount account;
+
+  function setUp() public override {
+    super.setUp();
+
+    beneficiary = payable(makeAddr("beneficiary"));
+    paymasterOperator = makeAddr("paymasterOperator");
+    (user, userKey) = makeAddrAndKey("user");
+    (guarantor, guarantorKey) = makeAddrAndKey("guarantor");
+    entryPoint = new EntryPoint();
+    entryPointSimulations = new EntryPointSimulations();
+    accountFactory = new SimpleAccountFactory(entryPoint);
+    paymaster = IWorld(worldAddress);
+    account = accountFactory.createAccount(user, 0);
+    counter = new TestCounter();
+
+    vm.prank(NamespaceOwner.get(ROOT_NAMESPACE_ID));
+    EntryPointTable.set(address(entryPoint));
+
+    // FOR TESTING, REMOVE LATER
+    vm.deal(paymasterOperator, 1000e18);
+    vm.startPrank(paymasterOperator);
+    entryPoint.depositTo{ value: 100e18 }(address(paymaster));
+    vm.stopPrank();
+  }
+
+  // sanity check for everything works without paymaster
+  function testCall() external {
+    vm.deal(address(account), 1e18);
+    PackedUserOperation memory op = fillUserOp(
+      account,
+      userKey,
+      address(counter),
+      0,
+      abi.encodeWithSelector(TestCounter.count.selector)
+    );
+    op.signature = signUserOp(op, userKey);
+    PackedUserOperation[] memory ops = new PackedUserOperation[](1);
+    ops[0] = op;
+    entryPoint.handleOps(ops, beneficiary);
+
+    // TODO: test the counter is increased and sender is correct
+  }
+
+  function testPaymaster() external {
+    vm.deal(address(account), 1e18);
+    PackedUserOperation memory op = fillUserOp(
+      account,
+      userKey,
+      address(counter),
+      0,
+      abi.encodeWithSelector(TestCounter.count.selector)
+    );
+    op.paymasterAndData = abi.encodePacked(address(paymaster), uint128(100000), uint128(100000));
+    op.signature = signUserOp(op, userKey);
+    submitUserOp(op);
+
+    // TODO: test the counter is increased and sender is correct
+  }
+
+  function fillUserOp(
+    SimpleAccount _sender,
+    uint256 _key,
+    address _to,
+    uint256 _value,
+    bytes memory _data
+  ) public view returns (PackedUserOperation memory op) {
+    op.sender = address(_sender);
+    op.nonce = entryPoint.getNonce(address(_sender), 0);
+    op.callData = abi.encodeWithSelector(SimpleAccount.execute.selector, _to, _value, _data);
+    op.accountGasLimits = bytes32(abi.encodePacked(bytes16(uint128(80000)), bytes16(uint128(50000))));
+    op.preVerificationGas = 50000;
+    op.gasFees = bytes32(abi.encodePacked(bytes16(uint128(100)), bytes16(uint128(1000000000))));
+    op.signature = signUserOp(op, _key);
+    return op;
+  }
+
+  function signUserOp(PackedUserOperation memory op, uint256 _key) public view returns (bytes memory signature) {
+    bytes32 hash = entryPoint.getUserOpHash(op);
+    (uint8 v, bytes32 r, bytes32 s) = vm.sign(_key, MessageHashUtils.toEthSignedMessageHash(hash));
+    signature = abi.encodePacked(r, s, v);
+  }
+
+  function submitUserOp(PackedUserOperation memory op) public {
+    PackedUserOperation[] memory ops = new PackedUserOperation[](1);
+    ops[0] = op;
+    entryPoint.handleOps(ops, beneficiary);
+  }
+}
